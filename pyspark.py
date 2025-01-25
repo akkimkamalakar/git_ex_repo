7588a51a1f53af8ddf96af2ce9622f4e531427b1
akkimkamalakar <55040820+akkimkamalakar@users.noreply.github.com>
2025-01-25 18:22:27 +0530

    6th commit

--- pyspark.py
+++ pyspark.py
@@ -3,4 +3,8 @@ spark=SparkSession.builder.getOrCreate()
 
 df=spark.read.csv('s3://this is s3 path')
 df.show()
+
+df2=spark.read.csv('s3//pth2')
+
+df3=df.join(df2,how='inner',on='df.id')
 print(spark)
